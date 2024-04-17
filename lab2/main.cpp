4d259449c6672c42764ba48cbd0a98e93effdc79
r4qq <64530515+r4qq@users.noreply.github.com>
2024-04-17 23:43:51 +0200

    ostatnie poprawki

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <utility>
+
 
 void showArray(int* array, int arraySize)
 {
@@ -26,41 +28,161 @@ void instertToArray(int* array, int arraySize)
         std::cin >> x;
         array[i] = x;
     }
-    //znak zakończenia tablicy 
-    array[arraySize] = '\0';
 }
 
-void arrayPop(int* &array, int &arraySize)
+void arrayPop(int* array, int& arraySize)
 {
     //pętla przesuwająca elementy o jeden w lewo 
-    for (int i = 0; i < arraySize + 1; i++)
+    for (int i = 0; i < arraySize - 1; ++i)
     {
         array[i] = array[i + 1];
     }
-    arraySize--; //zmniejszanie wielkośći tablicy o 1
-    array[arraySize] = '\0'; //znak końca talicy
-    array[arraySize + 1] = NULL; 
+    --arraySize; //zmniejszanie wielkośći tablicy o 1 
+}
+
+void swapDown(int* heap, int i, int heapSize)
+{
+    int max = i;
+    int leftElement = (2 * i) + 1;
+    int rightElement = (2 * i) + 2;
+    //sprawdzanie czy lewy potomek jest większy
+    if (leftElement < heapSize && heap[leftElement] > heap[max])
+    {
+        max = leftElement;
+    }
+    //sprawdzanie czy prawy potomek jest większy
+    if (rightElement < heapSize && heap[rightElement] > heap[max])
+    {
+        max = rightElement;
+    }
+
+    //zamień miejscami jeśli znaleziono większego potomka
+    if (max != i) 
+    {
+        std::swap(heap[i], heap[max]);
+        swapDown(heap, max, heapSize);
+    }
+}
+
+void heapify(int* heap, int heapSize)
+{
+    //Przechodzenie przez elementy od połowy korzenia
+    for (int i = heapSize / 2; i >= 0; --i)
+    {
+        swapDown(heap, i, heapSize);
+    }
+}
+
+void heapInsert(int*& heap, int& heapSize, int element)
+{
+    ++heapSize; // rozmiar kopca więlszy o 1
+    int* newHeap = new int[heapSize]; //alokacja pamieci nowego kopca
+
+    //kopiowanie elementów do nowego kopca
+    for (int i = 0; i < heapSize - 1; ++i) 
+    {
+        newHeap[i] = heap[i];
+    }
+
+    newHeap[heapSize - 1] = element; //wstawienie nowego elementu na ostatnie miejsce w kopcu
+
+    int x = heapSize - 1;
+
+    //przsuwanie nowego elementu na ostatnie miejsce kopca
+    while (x > 0 && newHeap[x] > newHeap[(x - 1)/2]) 
+    {
+        std::swap(newHeap[x], newHeap[(x - 1)/2]);
+        x = (x - 1) / 2;
+    } 
+    delete[] heap; // zwolnienie pamieci po startym kopcu
+    heap = newHeap; // przypisanie nowego kopca
+    //delete[] newHeap; 
+}
+
+void heap_pop(int*& heap, int& heapSize)
+{
+    heap[0] = heap[(heapSize - 1)]; // zamiana korzenia z ostatnim elementem kopca
+    heapSize--; // zmiejszenie rozmiaru o 1
+
+    int* newHeap = new int[heapSize]; // alokacja pamieci na nowy kopiec
+
+    //kopiowanie elementow do nowego kopca (oprocz ostatniego)
+    for (int i = 0; i < heapSize; ++i) 
+    {
+        newHeap[i] = heap[i];
+    }
+
+    delete[] heap; // zwolnienie pamieci po kopcu
+    heap = newHeap; //przypisanie nowego kopca
+    heapify(heap, heapSize);      //naprawa kopca
+}
+
+void heapSort(int* heap, int heapSize)
+{
+    heapify(heap, heapSize); // budowanie kopca
+
+    //wyciaganie elementow po jednym i ponowne budowanie kopca
+    for (int i = heapSize - 1; i > 0; i--) {
+        std::swap(heap[0], heap[i]); // zamiana korzenia (największego elementu) z ostatnim elementem tablicy
+        heapify(heap, heapSize); // budowa kopca
+    }
 }
 
 int main()
 {
      //tworzenie tablicy
     int wielkoscTablicy;
-    std::cout << "Podaj wilekość tablicy" << '\n';
+    std::cout << "Podaj wilekość kopca" << '\n';
     std::cin >> wielkoscTablicy;
     int* stworzonaTablica = makeArray(wielkoscTablicy);
 
     //wypełnianie tablicy
     std::cout << "Podaj liczby" << '\n';
     instertToArray(stworzonaTablica, wielkoscTablicy);
-
-    showArray(stworzonaTablica, wielkoscTablicy);
-
-    arrayPop(stworzonaTablica, wielkoscTablicy);
-
-    showArray(stworzonaTablica, wielkoscTablicy);
-
-    //zwalnianie pamięci (usuwanie tablicy)
-    delete[] stworzonaTablica; 
+    
+    //petla menu
+    while (true) 
+    {
+        int wybor;
+        std::cin >> wybor;
+        switch (wybor) 
+        {
+            default:
+                break;
+            case 1:
+                //dodawanie elementu do kopca
+                std::cout << "Daj element" << '\n';
+                int element;
+                std::cin >> element;
+                heapInsert(stworzonaTablica, wielkoscTablicy, element);
+                std::cout << "Nowy kopiec: " << '\n';
+                showArray(stworzonaTablica, wielkoscTablicy); //wyswietlanie kopca w formie tablicy
+                break;
+            case 2:    
+                //usuwanie pierwszego elementu z kopca
+                std::cout << "Nowy kopiec po popie" << '\n';
+                heap_pop(stworzonaTablica, wielkoscTablicy);
+                showArray(stworzonaTablica, wielkoscTablicy); //wyswietlanie kopca w formie tablicy
+                break;
+            case 3:
+                //heapsort
+                std::cout << "Heapsort: " << '\n';
+                heapSort(stworzonaTablica, wielkoscTablicy);
+                showArray(stworzonaTablica, wielkoscTablicy);//wyswietlanie kopca w formie tablicy
+                break;
+            case 4:
+                //usuwanie pierwszego elementu z listy
+                std::cout << "Test arrayPopa" << "\n";
+                arrayPop(stworzonaTablica, wielkoscTablicy);
+                showArray(stworzonaTablica, wielkoscTablicy); //wyswietlanie kopca w formie tablicy
+                break;
+            case 5:
+                //zwalnianie pamięci (usuwanie tablicy)
+                delete[] stworzonaTablica; 
+                //wyjście z programu
+                exit(0);
+        }
+    }
+    
     return 0;
 }
